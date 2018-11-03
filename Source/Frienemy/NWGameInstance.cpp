4d8f6d6261109f4fa57221837b115a3d0e0045a7
pass9frank <pass9frank02@sina.com>
2018-11-03 17:13:45 +0800

    配置文件提交2

--- Source/Frienemy/NWGameInstance.cpp
+++ Source/Frienemy/NWGameInstance.cpp
@@ -2,239 +2,239 @@
 
 #include "NWGameInstance.h"
 
-bool UNWGameInstance::HostSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, bool bIsLan, bool bIsPresence, int32 MaxNumPlayers)
-{
-	// Get the Online Subsystem to work with
-	IOnlineSubsystem* const OnlineSub = IOnlineSubsystem::Get();
-
-	if (OnlineSub)
-	{
-		// Get the Session Interface, so we can call the "CreateSession" function on it
-		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
-
-		if (Sessions.IsValid() && UserId.IsValid())
-		{
-			/*
-			Fill in all the Session Settings that we want to use.
-
-			There are more with SessionSettings.Set(...);
-			For example the Map or the GameMode/Type.
-			*/
-			SessionSettings = MakeShareable(new FOnlineSessionSettings());
-
-			SessionSettings->bIsLANMatch = bIsLan;
-			SessionSettings->bUsesPresence = bIsPresence;
-			SessionSettings->NumPublicConnections = MaxNumPlayers;
-			SessionSettings->NumPrivateConnections = 0;
-			SessionSettings->bAllowInvites = true;
-			SessionSettings->bAllowJoinInProgress = true;
-			SessionSettings->bShouldAdvertise = true;
-			SessionSettings->bAllowJoinViaPresence = true;
-			SessionSettings->bAllowJoinViaPresenceFriendsOnly = false;
-
-			SessionSettings->Set(SETTING_MAPNAME, FString("NewMap"), EOnlineDataAdvertisementType::ViaOnlineService);
-
-			// Set the delegate to the Handle of the SessionInterface
-			OnCreateSessionCompleteDelegateHandle = Sessions->AddOnCreateSessionCompleteDelegate_Handle(OnCreateSessionCompleteDelegate);
-
-			// Our delegate should get called when this is complete (doesn't need to be successful!)
-			return Sessions->CreateSession(*UserId, SessionName, *SessionSettings);
-		}
-	}
-	else
-	{
-		GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, TEXT("No OnlineSubsytem found!"));
-	}
-
-	return false;
-}
-
-void UNWGameInstance::FindSessions(TSharedPtr<const FUniqueNetId> UserId, bool bIsLAN, bool bIsPresence)
-{
-	IOnlineSubsystem * OnlineSub = IOnlineSubsystem::Get();
-
-	if (OnlineSub)
-	{
-		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
-
-		if (Sessions.IsValid() && UserId.IsValid())
-		{
-			SessionSearch = MakeShareable(new FOnlineSessionSearch());
-
-			SessionSearch->bIsLanQuery = bIsLAN;
-			SessionSearch->MaxSearchResults = 20;
-			SessionSearch->PingBucketSize = 50;
-
-			if (bIsPresence)
-			{
-				SessionSearch->QuerySettings.Set(SEARCH_PRESENCE, bIsPresence, EOnlineComparisonOp::NotEquals);
-			}
-
-			TSharedRef<FOnlineSessionSearch> SearchSettingsRef = SessionSearch.ToSharedRef();
-
-			OnFindSessionsCompleteDelegateHandle = Sessions->AddOnFindSessionsCompleteDelegate_Handle(OnFindSessionsCompleteDelegate);
-
-			Sessions->FindSessions(*UserId, SearchSettingsRef);
-		}
-	}
-	else
-	{
-		OnFindSessionsComplete(false);
-	}
-}
-
-bool UNWGameInstance::JoinSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, const FOnlineSessionSearchResult & SearchResult)
-{
-	bool bSuccessful = false;
-
-	IOnlineSubsystem * OnlineSub = IOnlineSubsystem::Get();
-
-	if (OnlineSub)
-	{
-		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
-
-		if (Sessions.IsValid() && UserId.IsValid())
-		{
-			OnJoinSessionCompleteDelegateHandle = Sessions->AddOnJoinSessionCompleteDelegate_Handle(OnJoinSessionCompleteDelegate);
-
-			bSuccessful = Sessions->JoinSession(*UserId, SessionName, SearchResult);
-		}
-	}
-	return bSuccessful;
-}
-
-UNWGameInstance::UNWGameInstance(const FObjectInitializer & ObjectInitializer)
-	:Super(ObjectInitializer)
-{
-	/************************************************************************/
-	/* Bind function for CREATING a Session                                                                     */
-	/************************************************************************/
-	OnCreateSessionCompleteDelegate = FOnCreateSessionCompleteDelegate::CreateUObject(this, &UNWGameInstance::OnCreateSessionComplete);
-	OnStartSessionCompleteDelegate = FOnStartSessionCompleteDelegate::CreateUObject(this, &UNWGameInstance::OnStartOnlineGameComplete);
-	OnFindSessionsCompleteDelegate = FOnFindSessionsCompleteDelegate::CreateUObject(this, &UNWGameInstance::OnFindSessionsComplete);
-	OnJoinSessionCompleteDelegate = FOnJoinSessionCompleteDelegate::CreateUObject(this, &UNWGameInstance::OnJoinSessionComplete);
-	OnDestroySessionCompleteDelegate = FOnDestroySessionCompleteDelegate::CreateUObject(this, &UNWGameInstance::OnDestroySessionComplete);
-}
-
-void UNWGameInstance::OnCreateSessionComplete(FName SessionName, bool bWasSuccessful)
-{
-	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("OnCreateSessionComplete %s, %d"), *SessionName.ToString(), bWasSuccessful));
-
-	// Get the OnlineSubsystem so we can get the Session Interface
-	IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get();
-	if (OnlineSub)
-	{
-		// Get the Session Interface to call the StartSession function
-		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
-
-		if (Sessions.IsValid())
-		{
-			// Clear the SessionComplete delegate handle, since we finished this call
-			Sessions->ClearOnCreateSessionCompleteDelegate_Handle(OnCreateSessionCompleteDelegateHandle);
-			if (bWasSuccessful)
-			{
-				// Set the StartSession delegate handle
-				OnStartSessionCompleteDelegateHandle = Sessions->AddOnStartSessionCompleteDelegate_Handle(OnStartSessionCompleteDelegate);
-
-				// Our StartSessionComplete delegate should get called after this
-				Sessions->StartSession(SessionName);
-			}
-		}
-
-	}
-}
-
-void UNWGameInstance::OnStartOnlineGameComplete(FName SessionName, bool bWasSuccessful)
-{
-	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("OnStartSessionComplete %s, %d"), *SessionName.ToString(), bWasSuccessful));
-
-	// Get the Online Subsystem so we can get the Session Interface
-	IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get();
-	if (OnlineSub)
-	{
-		// Get the Session Interface to clear the Delegate
-		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
-		if (Sessions.IsValid())
-		{
-			// Clear the delegate, since we are done with this call
-			Sessions->ClearOnStartSessionCompleteDelegate_Handle(OnStartSessionCompleteDelegateHandle);
-		}
-	}
-
-	// If the start was successful, we can open a NewMap if we want. Make sure to use "listen" as a parameter!
-	if (bWasSuccessful)
-	{
-		UGameplayStatics::OpenLevel(GetWorld(), "NewMap", true, "listen");
-	}
-}
-
-void UNWGameInstance::OnFindSessionsComplete(bool bWasSuccessful)
-{
-	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("FindSessionsComplete bSuccess: %d"), bWasSuccessful));
-
-	IOnlineSubsystem * const OnlineSub = IOnlineSubsystem::Get();
-
-	if (OnlineSub)
-	{
-		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
-		if (Sessions.IsValid())
-		{
-			Sessions->ClearOnFindSessionsCompleteDelegate_Handle(OnFindSessionsCompleteDelegateHandle);
-
-			GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("Num Search Results: %d"), SessionSearch->SearchResults.Num()));
-
-			if (SessionSearch->SearchResults.Num() > 0)
-			{
-				for (int32 SearchIdx = 0;SearchIdx < SessionSearch->SearchResults.Num();SearchIdx++)
-				{
-					GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("Session Number: %d | Sessionname: %s"), SearchIdx + 1, *(SessionSearch->SearchResults[SearchIdx].Session.OwningUserName)));
-				}
-			}
-		}
-	}
-}
-
-void UNWGameInstance::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
-{
-	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("OnJoinSessionComplete %s, %d"), *SessionName.ToString(), static_cast<int32>(Result)));
-
-	IOnlineSubsystem * OnlineSub = IOnlineSubsystem::Get();
-	if (OnlineSub)
-	{
-		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
-
-		if (Sessions.IsValid())
-		{
-			Sessions->ClearOnJoinSessionCompleteDelegate_Handle(OnJoinSessionCompleteDelegateHandle);
-
-			APlayerController * const PlayerController = GetFirstLocalPlayerController();
-
-			FString TravelURL;
-
-			if (PlayerController && Sessions->GetResolvedConnectString(SessionName, TravelURL))
-			{
-				PlayerController->ClientTravel(TravelURL, ETravelType::TRAVEL_Absolute);
-			}
-		}
-	}
-}
-
-void UNWGameInstance::OnDestroySessionComplete(FName SessionName, bool bWasSuccessful)
-{
-	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("OnDestroySessionComplete %s, %d"), *SessionName.ToString(), bWasSuccessful));
-
-	IOnlineSubsystem * OnlineSub = IOnlineSubsystem::Get();
-
-	if (OnlineSub)
-	{
-		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
-		if (Sessions.IsValid())
-		{
-			Sessions->ClearOnDestroySessionCompleteDelegate_Handle(OnDestroySessionCompleteDelegateHandle);
-			if (bWasSuccessful)
-			{
-				UGameplayStatics::OpenLevel(GetWorld(), "UIMap", true);
-			}
-		}
-	}
-}
+//bool UNWGameInstance::HostSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, bool bIsLan, bool bIsPresence, int32 MaxNumPlayers)
+//{
+//	// Get the Online Subsystem to work with
+//	IOnlineSubsystem* const OnlineSub = IOnlineSubsystem::Get();
+//
+//	if (OnlineSub)
+//	{
+//		// Get the Session Interface, so we can call the "CreateSession" function on it
+//		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
+//
+//		if (Sessions.IsValid() && UserId.IsValid())
+//		{
+//			/*
+//			Fill in all the Session Settings that we want to use.
+//
+//			There are more with SessionSettings.Set(...);
+//			For example the Map or the GameMode/Type.
+//			*/
+//			SessionSettings = MakeShareable(new FOnlineSessionSettings());
+//
+//			SessionSettings->bIsLANMatch = bIsLan;
+//			SessionSettings->bUsesPresence = bIsPresence;
+//			SessionSettings->NumPublicConnections = MaxNumPlayers;
+//			SessionSettings->NumPrivateConnections = 0;
+//			SessionSettings->bAllowInvites = true;
+//			SessionSettings->bAllowJoinInProgress = true;
+//			SessionSettings->bShouldAdvertise = true;
+//			SessionSettings->bAllowJoinViaPresence = true;
+//			SessionSettings->bAllowJoinViaPresenceFriendsOnly = false;
+//
+//			SessionSettings->Set(SETTING_MAPNAME, FString("NewMap"), EOnlineDataAdvertisementType::ViaOnlineService);
+//
+//			// Set the delegate to the Handle of the SessionInterface
+//			OnCreateSessionCompleteDelegateHandle = Sessions->AddOnCreateSessionCompleteDelegate_Handle(OnCreateSessionCompleteDelegate);
+//
+//			// Our delegate should get called when this is complete (doesn't need to be successful!)
+//			return Sessions->CreateSession(*UserId, SessionName, *SessionSettings);
+//		}
+//	}
+//	else
+//	{
+//		GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, TEXT("No OnlineSubsytem found!"));
+//	}
+//
+//	return false;
+//}
+//
+//void UNWGameInstance::FindSessions(TSharedPtr<const FUniqueNetId> UserId, bool bIsLAN, bool bIsPresence)
+//{
+//	IOnlineSubsystem * OnlineSub = IOnlineSubsystem::Get();
+//
+//	if (OnlineSub)
+//	{
+//		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
+//
+//		if (Sessions.IsValid() && UserId.IsValid())
+//		{
+//			SessionSearch = MakeShareable(new FOnlineSessionSearch());
+//
+//			SessionSearch->bIsLanQuery = bIsLAN;
+//			SessionSearch->MaxSearchResults = 20;
+//			SessionSearch->PingBucketSize = 50;
+//
+//			if (bIsPresence)
+//			{
+//				SessionSearch->QuerySettings.Set(SEARCH_PRESENCE, bIsPresence, EOnlineComparisonOp::NotEquals);
+//			}
+//
+//			TSharedRef<FOnlineSessionSearch> SearchSettingsRef = SessionSearch.ToSharedRef();
+//
+//			OnFindSessionsCompleteDelegateHandle = Sessions->AddOnFindSessionsCompleteDelegate_Handle(OnFindSessionsCompleteDelegate);
+//
+//			Sessions->FindSessions(*UserId, SearchSettingsRef);
+//		}
+//	}
+//	else
+//	{
+//		OnFindSessionsComplete(false);
+//	}
+//}
+//
+//bool UNWGameInstance::JoinSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, const FOnlineSessionSearchResult & SearchResult)
+//{
+//	bool bSuccessful = false;
+//
+//	IOnlineSubsystem * OnlineSub = IOnlineSubsystem::Get();
+//
+//	if (OnlineSub)
+//	{
+//		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
+//
+//		if (Sessions.IsValid() && UserId.IsValid())
+//		{
+//			OnJoinSessionCompleteDelegateHandle = Sessions->AddOnJoinSessionCompleteDelegate_Handle(OnJoinSessionCompleteDelegate);
+//
+//			bSuccessful = Sessions->JoinSession(*UserId, SessionName, SearchResult);
+//		}
+//	}
+//	return bSuccessful;
+//}
+//
+//UNWGameInstance::UNWGameInstance(const FObjectInitializer & ObjectInitializer)
+//	:Super(ObjectInitializer)
+//{
+//	/************************************************************************/
+//	/* Bind function for CREATING a Session                                                                     */
+//	/************************************************************************/
+//	//OnCreateSessionCompleteDelegate = FOnCreateSessionCompleteDelegate::CreateUObject(this, &UNWGameInstance::OnCreateSessionComplete);
+//	//OnStartSessionCompleteDelegate = FOnStartSessionCompleteDelegate::CreateUObject(this, &UNWGameInstance::OnStartOnlineGameComplete);
+//	//OnFindSessionsCompleteDelegate = FOnFindSessionsCompleteDelegate::CreateUObject(this, &UNWGameInstance::OnFindSessionsComplete);
+//	//OnJoinSessionCompleteDelegate = FOnJoinSessionCompleteDelegate::CreateUObject(this, &UNWGameInstance::OnJoinSessionComplete);
+//	//OnDestroySessionCompleteDelegate = FOnDestroySessionCompleteDelegate::CreateUObject(this, &UNWGameInstance::OnDestroySessionComplete);
+//}
+//
+//void UNWGameInstance::OnCreateSessionComplete(FName SessionName, bool bWasSuccessful)
+//{
+//	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("OnCreateSessionComplete %s, %d"), *SessionName.ToString(), bWasSuccessful));
+//
+//	// Get the OnlineSubsystem so we can get the Session Interface
+//	IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get();
+//	if (OnlineSub)
+//	{
+//		// Get the Session Interface to call the StartSession function
+//		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
+//
+//		if (Sessions.IsValid())
+//		{
+//			// Clear the SessionComplete delegate handle, since we finished this call
+//			Sessions->ClearOnCreateSessionCompleteDelegate_Handle(OnCreateSessionCompleteDelegateHandle);
+//			if (bWasSuccessful)
+//			{
+//				// Set the StartSession delegate handle
+//				OnStartSessionCompleteDelegateHandle = Sessions->AddOnStartSessionCompleteDelegate_Handle(OnStartSessionCompleteDelegate);
+//
+//				// Our StartSessionComplete delegate should get called after this
+//				Sessions->StartSession(SessionName);
+//			}
+//		}
+//
+//	}
+//}
+//
+//void UNWGameInstance::OnStartOnlineGameComplete(FName SessionName, bool bWasSuccessful)
+//{
+//	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("OnStartSessionComplete %s, %d"), *SessionName.ToString(), bWasSuccessful));
+//
+//	// Get the Online Subsystem so we can get the Session Interface
+//	IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get();
+//	if (OnlineSub)
+//	{
+//		// Get the Session Interface to clear the Delegate
+//		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
+//		if (Sessions.IsValid())
+//		{
+//			// Clear the delegate, since we are done with this call
+//			Sessions->ClearOnStartSessionCompleteDelegate_Handle(OnStartSessionCompleteDelegateHandle);
+//		}
+//	}
+//
+//	// If the start was successful, we can open a NewMap if we want. Make sure to use "listen" as a parameter!
+//	if (bWasSuccessful)
+//	{
+//		UGameplayStatics::OpenLevel(GetWorld(), "NewMap", true, "listen");
+//	}
+//}
+//
+//void UNWGameInstance::OnFindSessionsComplete(bool bWasSuccessful)
+//{
+//	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("FindSessionsComplete bSuccess: %d"), bWasSuccessful));
+//
+//	IOnlineSubsystem * const OnlineSub = IOnlineSubsystem::Get();
+//
+//	if (OnlineSub)
+//	{
+//		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
+//		if (Sessions.IsValid())
+//		{
+//			Sessions->ClearOnFindSessionsCompleteDelegate_Handle(OnFindSessionsCompleteDelegateHandle);
+//
+//			GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("Num Search Results: %d"), SessionSearch->SearchResults.Num()));
+//
+//			if (SessionSearch->SearchResults.Num() > 0)
+//			{
+//				for (int32 SearchIdx = 0;SearchIdx < SessionSearch->SearchResults.Num();SearchIdx++)
+//				{
+//					GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("Session Number: %d | Sessionname: %s"), SearchIdx + 1, *(SessionSearch->SearchResults[SearchIdx].Session.OwningUserName)));
+//				}
+//			}
+//		}
+//	}
+//}
+//
+//void UNWGameInstance::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+//{
+//	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("OnJoinSessionComplete %s, %d"), *SessionName.ToString(), static_cast<int32>(Result)));
+//
+//	IOnlineSubsystem * OnlineSub = IOnlineSubsystem::Get();
+//	if (OnlineSub)
+//	{
+//		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
+//
+//		if (Sessions.IsValid())
+//		{
+//			Sessions->ClearOnJoinSessionCompleteDelegate_Handle(OnJoinSessionCompleteDelegateHandle);
+//
+//			APlayerController * const PlayerController = GetFirstLocalPlayerController();
+//
+//			FString TravelURL;
+//
+//			if (PlayerController && Sessions->GetResolvedConnectString(SessionName, TravelURL))
+//			{
+//				PlayerController->ClientTravel(TravelURL, ETravelType::TRAVEL_Absolute);
+//			}
+//		}
+//	}
+//}
+//
+//void UNWGameInstance::OnDestroySessionComplete(FName SessionName, bool bWasSuccessful)
+//{
+//	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("OnDestroySessionComplete %s, %d"), *SessionName.ToString(), bWasSuccessful));
+//
+//	IOnlineSubsystem * OnlineSub = IOnlineSubsystem::Get();
+//
+//	if (OnlineSub)
+//	{
+//		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
+//		if (Sessions.IsValid())
+//		{
+//			Sessions->ClearOnDestroySessionCompleteDelegate_Handle(OnDestroySessionCompleteDelegateHandle);
+//			if (bWasSuccessful)
+//			{
+//				UGameplayStatics::OpenLevel(GetWorld(), "UIMap", true);
+//			}
+//		}
+//	}
+//}
--- Source/Frienemy/NWGameInstance.h
+++ Source/Frienemy/NWGameInstance.h
@@ -17,98 +17,98 @@ class FRIENEMY_API UNWGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 
-		/**
-		*	Function to host a game!
-		*
-		*	@Param		UserID			User that started the request
-		*	@Param		SessionName		Name of the Session
-		*	@Param		bIsLAN			Is this is LAN Game?
-		*	@Param		bIsPresence		"Is the Session to create a presence Session"
-		*	@Param		MaxNumPlayers	        Number of Maximum allowed players on this "Session" (Server)
-		*/
-		bool HostSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, bool bIsLan, bool bIsPresence, int32 MaxNumPlayers);
-	//UFUNCTION(BlueprintCallable, Category = "Sessions", meta = (DisplayName = "CreateSession"))
-	//	void HostSession_BP();
-	/************************************************************************/
-	/* Find an online session
-			@param UserId user that initiated the request
-			@param bIsLAN are we searching LAN matches
-			@param bIsPresence are we searching presence sessions
-	*/
-	/************************************************************************/
-	void FindSessions(TSharedPtr<const FUniqueNetId> UserId, bool bIsLAN, bool bIsPresence);
+	//	/**
+	//	*	Function to host a game!
+	//	*
+	//	*	@Param		UserID			User that started the request
+	//	*	@Param		SessionName		Name of the Session
+	//	*	@Param		bIsLAN			Is this is LAN Game?
+	//	*	@Param		bIsPresence		"Is the Session to create a presence Session"
+	//	*	@Param		MaxNumPlayers	        Number of Maximum allowed players on this "Session" (Server)
+	//	*/
+	//	bool HostSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, bool bIsLan, bool bIsPresence, int32 MaxNumPlayers);
+	////UFUNCTION(BlueprintCallable, Category = "Sessions", meta = (DisplayName = "CreateSession"))
+	////	void HostSession_BP();
+	///************************************************************************/
+	///* Find an online session
+	//		@param UserId user that initiated the request
+	//		@param bIsLAN are we searching LAN matches
+	//		@param bIsPresence are we searching presence sessions
+	//*/
+	///************************************************************************/
+	//void FindSessions(TSharedPtr<const FUniqueNetId> UserId, bool bIsLAN, bool bIsPresence);
 
-	/**
-	*	Joins a session via a search result
-	*
-	*	@param SessionName name of session
-	*	@param SearchResult Session to join
-	*
-	*	@return bool true if successful, false otherwise
-	*/
-	bool JoinSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, const FOnlineSessionSearchResult& SearchResult);
-	/* Delegate called when session created */
-	FOnCreateSessionCompleteDelegate OnCreateSessionCompleteDelegate;
+	///**
+	//*	Joins a session via a search result
+	//*
+	//*	@param SessionName name of session
+	//*	@param SearchResult Session to join
+	//*
+	//*	@return bool true if successful, false otherwise
+	//*/
+	//bool JoinSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, const FOnlineSessionSearchResult& SearchResult);
+	///* Delegate called when session created */
+	//FOnCreateSessionCompleteDelegate OnCreateSessionCompleteDelegate;
 
-	/* Delegate called when session started */
-	FOnStartSessionCompleteDelegate OnStartSessionCompleteDelegate;
+	///* Delegate called when session started */
+	//FOnStartSessionCompleteDelegate OnStartSessionCompleteDelegate;
 
-	/* Delegate for searching for sessions*/
-	FOnFindSessionsCompleteDelegate OnFindSessionsCompleteDelegate;
+	///* Delegate for searching for sessions*/
+	//FOnFindSessionsCompleteDelegate OnFindSessionsCompleteDelegate;
 
-	/** Delegate for joining a session */
-	FOnJoinSessionCompleteDelegate OnJoinSessionCompleteDelegate;
-	/** Delegate for destroying a session */
-	FOnDestroySessionCompleteDelegate OnDestroySessionCompleteDelegate;
+	///** Delegate for joining a session */
+	//FOnJoinSessionCompleteDelegate OnJoinSessionCompleteDelegate;
+	///** Delegate for destroying a session */
+	//FOnDestroySessionCompleteDelegate OnDestroySessionCompleteDelegate;
 
-	FDelegateHandle OnCreateSessionCompleteDelegateHandle;
-	FDelegateHandle OnStartSessionCompleteDelegateHandle;
-	FDelegateHandle OnFindSessionsCompleteDelegateHandle;
-	FDelegateHandle OnJoinSessionCompleteDelegateHandle;
-	FDelegateHandle OnDestroySessionCompleteDelegateHandle;
+	//FDelegateHandle OnCreateSessionCompleteDelegateHandle;
+	//FDelegateHandle OnStartSessionCompleteDelegateHandle;
+	//FDelegateHandle OnFindSessionsCompleteDelegateHandle;
+	//FDelegateHandle OnJoinSessionCompleteDelegateHandle;
+	//FDelegateHandle OnDestroySessionCompleteDelegateHandle;
 
-	TSharedPtr<class FOnlineSessionSettings> SessionSettings;
-	TSharedPtr<class FOnlineSessionSearch> SessionSearch;
+	//TSharedPtr<class FOnlineSessionSettings> SessionSettings;
+	//TSharedPtr<class FOnlineSessionSearch> SessionSearch;
 
-	UNWGameInstance(const FObjectInitializer& ObjectInitializer);
+	//UNWGameInstance(const FObjectInitializer& ObjectInitializer);
 
-	/**
-	*	Function fired when a session create request has completed
-	*
-	*	@param SessionName the name of the session this callback is for
-	*	@param bWasSuccessful true if the async action completed without error, false if there was an error
-	*/
-	virtual void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+	///**
+	//*	Function fired when a session create request has completed
+	//*
+	//*	@param SessionName the name of the session this callback is for
+	//*	@param bWasSuccessful true if the async action completed without error, false if there was an error
+	//*/
+	//virtual void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
 
 
-	/**
-	*	Function fired when a session start request has completed
-	*
-	*	@param SessionName the name of the session this callback is for
-	*	@param bWasSuccessful true if the async action completed without error, false if there was an error
-	*/
-	void OnStartOnlineGameComplete(FName SessionName, bool bWasSuccessful);
+	///**
+	//*	Function fired when a session start request has completed
+	//*
+	//*	@param SessionName the name of the session this callback is for
+	//*	@param bWasSuccessful true if the async action completed without error, false if there was an error
+	//*/
+	//void OnStartOnlineGameComplete(FName SessionName, bool bWasSuccessful);
 
-	/**
-	*	Delegate fired when a session search query has completed
-	*
-	*	@param bWasSuccessful true if the async action completed without error, false if there was an error
-	*/
-	void OnFindSessionsComplete(bool bWasSuccessful);
-	
-	/**
-	*	Delegate fired when a session join request has completed
-	*
-	*	@param SessionName the name of the session this callback is for
-	*	@param bWasSuccessful true if the async action completed without error, false if there was an error
-	*/
-	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+	///**
+	//*	Delegate fired when a session search query has completed
+	//*
+	//*	@param bWasSuccessful true if the async action completed without error, false if there was an error
+	//*/
+	//void OnFindSessionsComplete(bool bWasSuccessful);
+	//
+	///**
+	//*	Delegate fired when a session join request has completed
+	//*
+	//*	@param SessionName the name of the session this callback is for
+	//*	@param bWasSuccessful true if the async action completed without error, false if there was an error
+	//*/
+	//void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
 
-	/**
-	*	Delegate fired when a destroying an online session has completed
-	*
-	*	@param SessionName the name of the session this callback is for
-	*	@param bWasSuccessful true if the async action completed without error, false if there was an error
-	*/
-	virtual void OnDestroySessionComplete(FName SessionName, bool bWasSuccessful);
+	///**
+	//*	Delegate fired when a destroying an online session has completed
+	//*
+	//*	@param SessionName the name of the session this callback is for
+	//*	@param bWasSuccessful true if the async action completed without error, false if there was an error
+	//*/
+	//virtual void OnDestroySessionComplete(FName SessionName, bool bWasSuccessful);
 };
