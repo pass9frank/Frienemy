a8059c9fd75073f098bf5402b4aa55d33bd62ba4
pass9frank <pass9frank02@sina.com>
2018-11-08 19:31:28 +0800

    联网测试跑通

--- Build/Android/src/com/YourCompany/Frienemy/OBBData.java
+++ Build/Android/src/com/YourCompany/Frienemy/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-68499393L // the length of the file in bytes
+68516305L // the length of the file in bytes
 )
 };
 };
